b06357e2fc381cea7aa690051a38af55f2ed5b3b
Neil Locketz <neil@nlocketz.com>
2017-02-14 22:20:00 -0500

    BPHC-41: More tests done

--- pli/login_form.py
+++ pli/login_form.py
@@ -2,4 +2,4 @@ from wtforms import Form, StringField, PasswordField, validators
 
 class LoginForm(Form):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
--- tests/session_auth_test.py
+++ tests/session_auth_test.py
@@ -1,4 +1,4 @@
-from application import application as app
+from application import application as pli
 import mongomock
 from pli import validate_login
 import unittest
@@ -40,17 +40,77 @@ def mocked_users():
     collection.insert_many(users)
     return collection
 
+def check_index_page(f, r):
+    f("PLI has answers to all your health-related questions" in r.data)
+    f("Index-Page" in r.data)
+    f("Choose a category or search below." in r.data)
+
+def assert_index_page(tr, r):
+    check_index_page(lambda x: tr.assertTrue(x, "Is index page"), r)
+def assert_not_index_page(tr, r):
+    check_index_page(lambda x: tr.assertFalse(x, "Not index page"), r)
+    
+def check_login_page(f, r):
+    # I'm satisfied with these deciding this is the login page
+    f("Email Address" in r.data)
+    f("Login" in r.data)
+    f("Password" in r.data)
+
+def assert_login_page(tr, r):
+    check_login_page(lambda x: tr.assertTrue(x, "Is login page"), r)
+
+def assert_not_login_page(tr, r):
+    check_login_page(tr.assertFalse(x, "Is not login page"), r)
+
+    
 class LoginTestCase(unittest.TestCase):
 
     def setUp(self):
-        app.config['db'] = mocked_users()
-        self.app = app.test_client()
+        pli.config['db'] = mocked_users()
+        self.app = pli.test_client()
 
     def test_good_login(self):
-        with app.app_context():
-            self.assertEqual(user1["_id"],
-                             validate_login(user1["email_address"], user1["password"]))
+        with pli.app_context():
+            self.assertEqual(user1["_id"],validate_login(user1["email_address"], user1["password"]))
+            self.assertEqual(user2["_id"],validate_login(user2["email_address"], user2["password"]))
+
     def test_bad_login(self):
-        with app.app_context():
-            self.assertEqual(user1["_id"],
-                             validate_login(user2["email_address"], user1["password"]))
+        with pli.app_context():
+            self.assertEqual(None,validate_login(32, user1["password"]))
+            self.assertEqual(None,validate_login(user2["email_address"], user1["password"]))
+            self.assertEqual(None,validate_login(user3["email_address"], user1["password"]))
+            self.assertEqual(None,validate_login(43.2, 4))
+            self.assertEqual(None,validate_login("{ $gt : \"*\" }", "{ $gt : \"*\" }"))
+            self.assertEqual(None,validate_login(user2["email_address"], user3["password"]))
+            self.assertEqual(None,validate_login("", ""))
+            self.assertEqual(None,validate_login(None, None))
+
+    def test_login_page(self):
+        with pli.app_context():
+            r = self.app.get("/login")
+            assert_login_page(self, r)
+
+    def test_good_login_post(self):
+        with pli.app_context():
+            assert_index_page(self, self.do_login(user1["email_address"], user1["password"]))
+            assert_index_page(self, self.do_login(user2["email_address"], user2["password"]))
+            assert_index_page(self, self.do_login(user3["email_address"], user3["password"]))
+
+    def test_bad_login_post(self):
+        with pli.app_context():
+            assert_login_page(self, self.do_login(user1["email_address"], user2["password"]))
+            assert_login_page(self, self.do_login(user2["email_address"], user3["password"]))
+            assert_login_page(self, self.do_login(user3["email_address"], user1["password"]))
+
+    def do_login(self, user, passwd, to=None):
+
+        if to is not None:
+            param = "?next=%s" % str(to)
+        else:
+            param = ""
+            
+        return self.app.post('/login'+param, data=dict(
+            email=user,
+            password=passwd
+        ), follow_redirects=True)
+
