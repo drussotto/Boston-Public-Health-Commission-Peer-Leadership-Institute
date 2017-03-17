c001fd26781114027a5f39d79d8406f1f3b74f72
Neil Locketz <neil@nlocketz.com>
2017-03-16 20:37:32 -0400

    Missed one

--- pli/carousel_card/set_wn_cards_form.py
+++ pli/carousel_card/set_wn_cards_form.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 from wtforms import Form, StringField, IntegerField, validators, FieldList
 
 class SetWnCardsForm(Form):
     cards = FieldList(StringField("ids"), min_entries=3)
-=======
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
-from wtforms import StringField, IntegerField, validators, FieldList
-
-class SetWnCardsForm(FlaskForm):
-    cards = FieldList(StringField("ObjIds"), min_entries=1)
->>>>>>> fb0b451... Cards are rendered mock stuff is a little wonkey
--- pli/carousel_card/wn_card_form.py
+++ pli/carousel_card/wn_card_form.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from wtforms import StringField, IntegerField, validators, Form, FileField
 
 class WnCardInfoAddForm(Form):
@@ -9,20 +8,6 @@ class WnCardInfoAddForm(Form):
 
     def extract(self):
         return {
-=======
-
-from wtforms import StringField, IntegerField, validators, Form, FileField
-
-class WnCardInfoAddForm(Form):
-    background = FileField("Background Image")
-    caption = StringField('Image caption')
-    sub_caption = StringField('Image sub-caption')
-    hyperlink = StringField('Link')
-
-    def extract(self):
-        return {
-            "background": self.background,
->>>>>>> fb0b451... Cards are rendered mock stuff is a little wonkey
             "caption": self.caption.data,
             "sub_caption": self.sub_caption.data,
             "hyperlink": self.hyperlink.data
