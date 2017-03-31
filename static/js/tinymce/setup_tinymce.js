8af2f0a6e8e5ac08566a6ec8112be12e8de3a4ad
Neil Locketz <neil@nlocketz.com>
2017-03-30 21:14:38 -0400

    Fix again

--- static/js/tinymce/setup_tinymce.js
+++ static/js/tinymce/setup_tinymce.js
@@ -4,10 +4,8 @@ $(function() {
         // height: '100px',
         // Location of TinyMCE script
         script_url : "/static/js/tinymce/tinymce.min.js",
-
-        // General options
-        plugins : "emoticons,autoresize,pagebreak,table,save,preview,media,searchreplace,print,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking",
-	toolbar : "undo,redo,|,styleselect,|,formats,|,bold,italic,|,alighleft,aligncenter,alignright,alignjustify,|,bullist,numlist,outdent,indent,|,table,|,fontsizeselect,|,emoticons",
+	// General options
+        plugins : "image,emoticons,autoresize,pagebreak,save,preview,media,searchreplace,print,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking,textcolor,colorpicker,advlist,lists",			toolbar : "undo,redo,|,styleselect,|,formats,|,bold,italic,|,alighleft,aligncenter,alignright,|,bullist,numlist,outdent,indent,|,fontsizeselect,|,emoticons,|,image,|,preview,|,forecolor,backcolor",
         theme_modern_resizing : true,
 	autoresize_overflow_padding: 50,
     });
