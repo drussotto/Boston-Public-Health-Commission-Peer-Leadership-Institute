5d30ad923e10f1f621c609f470dce3b74970da51
Alex Berberi <alexlberberi@gmail.com>
2017-03-31 10:57:32 -0400

    BPHC-125: layout correctly enforces center column content

--- static/js/tinymce/setup_tinymce.js
+++ static/js/tinymce/setup_tinymce.js
@@ -1,13 +1,14 @@
 $(function() {
     $('textarea.tinymce').tinymce({
-	// width: '500px',
+	      // width: '500px',
         // height: '100px',
         // Location of TinyMCE script
         script_url : "/static/js/tinymce/tinymce.min.js",
-	// General options
-        plugins : "image,emoticons,autoresize,pagebreak,save,preview,media,searchreplace,print,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking,textcolor,colorpicker,advlist,lists",			toolbar : "undo,redo,|,styleselect,|,formats,|,bold,italic,|,alighleft,aligncenter,alignright,|,bullist,numlist,outdent,indent,|,fontsizeselect,|,emoticons,|,image,|,preview,|,forecolor,backcolor",
+	      // General options
+        plugins : "image,emoticons,autoresize,pagebreak,save,preview,media,searchreplace,print,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking,textcolor,colorpicker,advlist,lists",
+  			toolbar : "undo,redo,|,styleselect,|,formats,|,bold,italic,|,alighleft,aligncenter,alignright,|,bullist,numlist,outdent,indent,|,fontsizeselect,|,emoticons,|,image,|,preview,|,forecolor,backcolor",
         theme_modern_resizing : true,
-	autoresize_overflow_padding: 50,
+	      autoresize_overflow_padding: 50,
     });
 });
 
